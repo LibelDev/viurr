25684f4e2e58b1dd09ac5d4de41cd898c12071fa
kitce <kitce.dev@gmail.com>
2018-07-16 03:08:19 +0800

    Not to encode if the file already exists

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,20 +1,30 @@
 import Debugger from 'debug';
 import ffmpeg from 'fluent-ffmpeg';
+import fs from 'fs';
+import util from 'util';
+import exists from './exists';
 
 const debug = Debugger('viuer:lib:ffmpeg');
 
-export const encode = (url: string, filePath: string) => {
-  const command = ffmpeg(url)
-    .videoCodec('copy')
-    .audioCodec('copy')
-    .save(filePath);
-  command.on('start', () => {
-    debug('start encoding', filePath);
-  });
-  command.on('end', () => {
-    debug('finished :', filePath);
-  });
-  return new Promise((resolve, reject) => {
+const access = util.promisify(fs.access);
+
+export const encode = (url: string, filePath: string): Promise<void> => {
+  return new Promise(async (resolve, reject) => {
+    if (await exists(filePath)) {
+      const err = new Error(`${filePath} already exists`);
+      return reject(err);
+    }
+    const command = ffmpeg(url)
+      .videoCodec('copy')
+      .audioCodec('copy')
+      .save(filePath);
+    command.on('start', (commandLine) => {
+      debug('start encoding', filePath);
+      debug('command :', commandLine);
+    });
+    command.on('end', () => {
+      debug('finished :', filePath);
+    });
     command.on('end', () => resolve());
     command.on('error', reject);
   });
